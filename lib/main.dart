7ac344ed68e94f46d14855ad3ecc07e58303f425
Taifur Rahaman <taifur.rahaman1821@gmail.com>
2021-07-03 17:32:19 +0600

    quiz file is added

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:quiz_app/quiz.dart';
+import 'package:quiz_app/widget/mytheme.dart';
 
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: MyTheme.myTheme(context),
+      home: QuizMain(),
+    );
+  }
+}
