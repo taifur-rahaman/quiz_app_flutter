c7c95efd24c0d5260481e4718df763559ae90fff
Taifur Rahaman <taifur.rahaman1821@gmail.com>
2021-07-03 23:11:47 +0600

    scorekeeper added

--- lib/quiz.dart
+++ lib/quiz.dart
@@ -1,70 +1,98 @@
 import 'package:flutter/material.dart';
 
-class QuizMain extends StatelessWidget {
+class QuizMain extends StatefulWidget {
+  @override
+  _QuizMainState createState() => _QuizMainState();
+}
+
+class _QuizMainState extends State<QuizMain> {
+  List<Widget> scoreKeeper = [];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SafeArea(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: <Widget>[
-            Expanded(
-              flex: 5,
-              child: Center(
-                child: Padding(
-                  padding: const EdgeInsets.symmetric(
-                    horizontal: 10,
-                  ),
-                  child: Text(
-                    'This is where the question text will go',
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 25,
-                    ),
-                    textAlign: TextAlign.center,
-                  ),
-                ),
-              ),
-            ),
-            Expanded(
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: <Widget>[
+          Expanded(
+            flex: 5,
+            child: Center(
               child: Padding(
-                padding: const EdgeInsets.all(15.0),
-                child: TextButton(
-                  child: Text(
-                    'True',
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 18,
-                    ),
+                padding: const EdgeInsets.symmetric(
+                  horizontal: 10,
+                ),
+                child: Text(
+                  'This is where the question text will go',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 25,
                   ),
-                  onPressed: () {},
-                  style: TextButton.styleFrom(
-                    backgroundColor: Colors.green,
-                  ),
+                  textAlign: TextAlign.center,
                 ),
               ),
             ),
-            Expanded(
-              child: Padding(
-                padding: const EdgeInsets.all(15.0),
-                child: TextButton(
-                  child: Text(
-                    'False',
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 18,
-                    ),
+          ),
+          Expanded(
+            child: Padding(
+              padding: const EdgeInsets.all(15.0),
+              child: TextButton(
+                child: Text(
+                  'True',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 18,
                   ),
-                  onPressed: () {},
-                  style: TextButton.styleFrom(
-                    backgroundColor: Colors.red,
-                  ),
+                ),
+                onPressed: () {
+                  setState(
+                    () {
+                      scoreKeeper.add(
+                        Icon(
+                          Icons.check,
+                          color: Colors.green,
+                        ),
+                      );
+                    },
+                  );
+                },
+                style: TextButton.styleFrom(
+                  backgroundColor: Colors.green,
                 ),
               ),
             ),
-          ],
-        ),
+          ),
+          Expanded(
+            child: Padding(
+              padding: const EdgeInsets.all(15.0),
+              child: TextButton(
+                child: Text(
+                  'False',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 18,
+                  ),
+                ),
+                onPressed: () {
+                  setState(() {
+                    scoreKeeper.add(
+                      Icon(
+                        Icons.close,
+                        color: Colors.red,
+                      ),
+                    );
+                  });
+                },
+                style: TextButton.styleFrom(
+                  backgroundColor: Colors.red,
+                ),
+              ),
+            ),
+          ),
+          Row(
+            children: scoreKeeper,
+          )
+        ],
       ),
     );
   }
